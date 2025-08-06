a5b998e6a6837ee09bdfa53fdfb060aa8e7c899b
Ray-Gabe <raylin.sami@gmail.com>
2025-08-07 01:07:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1102,4 +1102,6 @@ def clear_user_data():
     return jsonify({'status': 'All user data cleared'})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
